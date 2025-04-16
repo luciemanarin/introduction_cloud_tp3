ccc08f1f7ad1478c9759f529a8d2aeac730fa23b
lucie <manarinlucie@gmail.com>
2025-04-16 17:14:25 +0200

    add: render.yaml corrigé

--- db.js
+++ db.js
@@ -1,26 +1,16 @@
 const { Sequelize } = require('sequelize')
 
-// database connection string provided by Render or other environment
-const databaseUrl = process.env.DATABASE_URL;
-
-// Check if DATABASE_URL is provided
-if (!databaseUrl) {
-  console.error('DATABASE_URL environment variable is not set.');
-  process.exit(1); // Exit if no database URL is found
-}
-
 const sequelize = new Sequelize(
-  databaseUrl, // Use the connection URL
+  process.env.DATABASE_URL,
+  
   {
-    // host, database, user, password will be extracted from the URL
     dialect: 'postgres',
     dialectOptions: {
       ssl: {
-        require: true, // Required for Render databases
-        rejectUnauthorized: false, // Required for Render databases
+        require: true,
+        rejectUnauthorized: false,
       },
     },
-    logging: false, // Optional: disable logging SQL queries
     define: {
       createdAt: 'added',
       updatedAt: 'updated',
@@ -29,12 +19,6 @@ const sequelize = new Sequelize(
 )
 
 sequelize.authenticate()
-  .then(() => console.log('Database connection established successfully.'))
-  .catch(err => console.error('Unable to connect to the database:', err));
-
 sequelize.sync()
-  .then(() => console.log('Models synchronized successfully.'))
-  .catch(err => console.error('Unable to synchronize models:', err));
-
 
 module.exports = sequelize
